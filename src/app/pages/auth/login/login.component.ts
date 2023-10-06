82391fe8bbe8bd8ee26596d7aed984a98d3078a6
ricardo-mendonca <ricardo-mendonca@live.com>
2023-10-06 20:04:37 -0300

    Atualização tela inicial

--- src/app/pages/auth/login/login.component.ts
+++ src/app/pages/auth/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { AuthService } from 'src/app/services/auth.service';
 import { LoginService } from 'src/app/services/login.service';
 
@@ -15,19 +16,21 @@ export class LoginComponent {
     public formBuilder: FormBuilder,
     private router: Router,
     private loginService: LoginService,
-    public authService: AuthService
+    public authService: AuthService,
+    private toastr: ToastrService
+  ) {}
 
-  ){}
-
-  loginForm: FormGroup ;
+  loginForm: FormGroup;
 
   ngOnInit(): void {
-    this.loginForm = this.formBuilder.group(
-      {
-        email: ['', [Validators.required, Validators.email]],
-        senha: ['', [Validators.required]],
-      }
-    );
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      senha: ['', [Validators.required]],
+    });
+  }
+
+  ShowError(resposta) {
+    this.toastr.error(resposta);
   }
 
   get dadosForm() {
@@ -39,19 +42,20 @@ export class LoginComponent {
       .login(this.dadosForm['email'].value, this.dadosForm['senha'].value)
       .subscribe(
         (ret) => {
-          console.log(ret);
-          console.log(ret["nome"]);
+          //console.log(ret);
+          //console.log(ret['nome']);
 
-          this.authService.setToken(ret["accessToken"].toString());
+          this.authService.setToken(ret['accessToken'].toString());
           this.authService.UsuarioAutenticado(true);
-          this.authService.setUsuarioName(ret["nome"]);
+          this.authService.setUsuarioName(ret['nome']);
           this.router.navigate(['/dashboard']);
         },
         (error) => {
           var resposta = error;
-        //console.log(resposta.error.message);
-        resposta = resposta.error.message;
-        alert(resposta);
+          //console.log(resposta.error.message);
+          resposta = resposta.error.message;
+          this.ShowError(resposta);
+          //alert(resposta);
         }
       );
   }
